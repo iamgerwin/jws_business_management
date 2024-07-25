6d27a1839ff682dab195cf2f321a571d0234bf89
iamgerwin <iamgerwin@live.com>
2024-07-26 02:22:32 +0800

    fix: simplify allowable type in api tasks show route

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,10 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\BusinessController;
 
-enum TaskableEnum: string {
-    case business = 'business';
-    case person = 'person';
-}
-
+//enum TaskableEnum: string {
+//    case business = 'business';
+//    case person = 'person';
+//}
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -21,6 +20,6 @@ Route::get('/user', function (Request $request) {
 Route::get('categories', CategoryController::class)->name('api.categories.index');
 Route::get('tags', TagController::class)->name('api.tags.index');
 Route::get('businesses', BusinessController::class)->name('api.businesses.index');
-Route::get('task/{type}/{id}/{status?}', TaskListController::class)->whereIn('type', TaskableEnum::cases())->name('api.task.show');
+Route::get('task/{type}/{id}/{status?}', TaskListController::class)->whereIn('type', ['business', 'person'])->name('api.task.show');
 Route::get('tasks/{status?}', TasksListController::class)->name('api.tasks.index');
 
